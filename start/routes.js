903c660191fc1865cf48409acb6cccb7b5895e6b
AdrianCore <alerion_core@hotmail.com>
2021-07-13 19:34:20 -0500

    Se crearon los usuarios y los proyectos, así como la relación entre dichas tablas

--- start/routes.js
+++ start/routes.js
@@ -16,6 +16,9 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.get('/', () => {
-  return { greeting: 'Hello world in JSON' }
-})
+Route.group(() => {
+  Route.post('users/register', 'UserController.store');
+  Route.post('users/login', 'UserController.login');
+  Route.get('projects', 'ProjectController.index').middleware('auth');
+  Route.post('projects', 'ProjectController.create').middleware('auth');
+}).prefix('api/v1/');
